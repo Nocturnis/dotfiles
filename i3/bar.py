3bac4de8f16460b993b326a63037397c71854227
Braden Walker <bradenw@google.com>
2015-02-03 13:46:22 -0800

    Correct private member naming

--- i3/bar.py
+++ i3/bar.py
@@ -20,23 +20,23 @@ class BarPart:
 # CPU
 class CpuPart(BarPart):
     def __init__(self):
-        self.mpstat__ = subprocess.Popen(['mpstat', '5'], stdout=subprocess.PIPE)
-        self.mpstat__.stdout.readline()
-        self.last_value__ = 0
+        self._mpstat = subprocess.Popen(['mpstat', '5'], stdout=subprocess.PIPE)
+        self._mpstat.stdout.readline()
+        self._last_value = 0
 
     def __mpstat_has_available(self):
-        return len(select.select([self.mpstat__.stdout], [], [], 0)[0]) > 0
+        return len(select.select([self._mpstat.stdout], [], [], 0)[0]) > 0
 
     def __status(self):
         last_line = None
         while self.__mpstat_has_available():
-            last_line = self.mpstat__.stdout.readline()
+            last_line = self._mpstat.stdout.readline()
         if not last_line is None:
             idle = float(re.search('all +' + ('[\.0-9]+ +' * 9) + '([\.0-9]+)', last_line).group(1)) / 100
             value = 1 - idle
-            self.last_value__ = value
+            self._last_value = value
             return value
-        return self.last_value__
+        return self._last_value
 
     def render(self, frame):
         cpu_usage = self.__status()
