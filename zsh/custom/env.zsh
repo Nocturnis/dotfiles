e17f2df97b901f632f641a9569fc80d8c6ab47d3
Braden Walker <braden.w@live.com>
2015-08-31 10:33:27 -0700

    Add mvim alias, remove lxc as vagrant default

--- zsh/custom/env.zsh
+++ zsh/custom/env.zsh
@@ -4,6 +4,10 @@ export P4DIFF=vimdiff
 export LSCOLORS=dxfxgxbxcxedabagacad
 export LS_COLORS="di=33:ln=35:so=36:pi=31:ex=32:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=1;37;42:ow=0;43:"
 export JAVA_HOME="/Library/Java/Home"
-export VAGRANT_DEFAULT_PROVIDER="lxc"
+
+# If mvim is available, use that so YCM works.
+if [[ -f /usr/local/Cellar/macvim/7.4-77/bin/mvim ]]; then
+  alias vim="/usr/local/Cellar/macvim/7.4-77/bin/mvim -v"
+fi
 
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
